65ea02b1634ac78847c06777cae46c22b53b2daa
graill <liam.grail@etu.univ-grenoble-alpes.fr>
2024-11-11 03:41:14 +0100

    buses complete

--- Man/src/client/Bus.js
+++ Man/src/client/Bus.js
@@ -3,16 +3,20 @@ class Bus {
 constructor(busTypeJSON, parcours,arrets, ctx){
     this.type = busTypeJSON.type;
     this.capacite = busTypeJSON["capacite-max"];
-    this.vitesseChargement = busTypeJSON["vitesse-chargement"];
-    this.vitesseDeplacement = busTypeJSON["vitesse-deplacement"];
+    this.vitesseDeplacement = 1/busTypeJSON["vitesse-deplacement"];
     this.parcours = [];
+    this.state;
     parcours.forEach(stop => {
         this.parcours.push([arrets[stop],-1])
     });
     this.arretCourrant = 0;
     this.distanceParcouru = 0;
-    this.pourcentageProgression = 0;
-    this.setParcoursDistances();
+    this.pourcentageProgression = 0; 
+    let retour = this.getShortestPath(arrets,this.parcours[this.parcours.length-1][0],this.parcours[0][0]).path;
+    for (let index = 1; index<retour.length; index++){
+        this.parcours.push([retour[index],-1]);
+    }
+    this.setParcoursDistances(arrets);
     this.distanceProchaineArret=this.parcours[this.arretCourrant][1];
     this.radius = 2;
     this.ctx = ctx;
@@ -20,17 +24,78 @@ constructor(busTypeJSON, parcours,arrets, ctx){
     this.y;
 }
 
-setParcoursDistances(){
+setParcoursDistances(arrets){
     for (let index = 0; index<this.parcours.length -1; index++){
-        this.parcours[index][1] = this.parcours[index][0].links[this.parcours[index+1][0].nom]
+        let next = this.parcours[index][0].links[this.parcours[index+1][0].nom];
+        if (next==undefined){
+            let inserts = this.getShortestPath(arrets,this.parcours[index][0],this.parcours[index+1][0]).path
+            for(let insert = 1; insert < inserts.length-1; insert++){
+                this.parcours.splice(index+insert,0,[inserts[insert],-1]);                         
+            }
+            this.setParcoursDistances(arrets);            
+            break;       
+        }
+        else{
+            this.parcours[index][1] = next;
+        }        
     }   
+    
+}
+
+getShortestPath(arrets,from,to){
+    const distances = {};
+    const previous = {};
+    const unvisited = [];
+
+    for (let arret of Object.values(arrets)) {
+      distances[arret.nom] = Infinity;
+      previous[arret.nom] = null;
+      unvisited.push(arret);
+    }
+    distances[from.nom] = 0;
+
+    while (unvisited.length > 0) {
+      unvisited.sort((a, b) => distances[a.nom] - distances[b.nom]);
+
+      const current = unvisited.shift();
+
+      if (current.nom === to.nom) break;
+
+      for (let [neighborName, distance] of Object.entries(current.links)) {
+        const neighbor = arrets[neighborName];
+        const altDistance = distances[current.nom] + distance;
+
+        if (altDistance < distances[neighbor.nom]) {
+          distances[neighbor.nom] = altDistance;
+          previous[neighbor.nom] = current;
+        }
+      }
+    }
+
+    const path = [];
+    let step = to;
+    while (step) {        
+      path.unshift(step);
+      step = previous[step.nom];
+    }
+
+    return {
+      path: path.length > 1 ? path : null,
+      distance: distances[to.nom] !== Infinity ? distances[to.nom] : null
+    };
 }
 
 setPosition(){
     let current = this.parcours[this.arretCourrant][0];
-    let next = this.parcours[this.arretCourrant + 1][0];
-    this.x = current.x + (current.x - next.x) * this.pourcentageProgression;
-    this.y = current.y + (current.y - next.y) * this.pourcentageProgression;
+    if(this.arretCourrant<this.parcours.length-1){        
+        let next = this.parcours[this.arretCourrant + 1][0];
+        this.x = current.x + (next.x - current.x) * this.pourcentageProgression;
+        this.y = current.y + (next.y - current.y) * this.pourcentageProgression;
+    }
+    else{
+        this.x = current.x;
+        this.y = current.y;
+    }
 }
 
 render(){
@@ -41,21 +106,24 @@ render(){
     this.ctx.stroke();
 }
 
-increment(){
+increment(){    
+    console.log(this.state);
+    if(this.state === "DEPLACEMENT"){        
     this.distanceParcouru += this.vitesseDeplacement;
-    this.pourcentageProgression = this.distanceProchaineArret/this.distanceParcouru;
+    this.pourcentageProgression = (this.distanceParcouru/this.distanceProchaineArret);
     if(this.distanceParcouru >= this.distanceProchaineArret){
         this.distanceParcouru = 0;
         this.pourcentageProgression = 0;
-        if(this.arretCourrant + 1 < this.parcours.length - 1){        
-            this.arretCourrant++   
+        if(this.arretCourrant + 1 < this.parcours.length){        
+            this.arretCourrant++;            
         }
         else{
-            this.arretCourrant = 0;
+            this.arretCourrant = 0;            
         }
         this.distanceProchaineArret=this.parcours[this.arretCourrant][1];        
     }
     this.setPosition();
+}
 }
 
 }
